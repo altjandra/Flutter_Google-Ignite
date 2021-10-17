149a3bd0919d98660698f4e7b3bc9da70a4ede30
charleneteo <charleneteo.2020@sis.smu.edu.sg>
2021-10-18 02:35:58 +0800

    login

--- the_basics/lib/main.dart
+++ the_basics/lib/main.dart
@@ -1,7 +1,8 @@
    
 import 'package:flutter/material.dart';
 import 'package:the_basics/views/home/home_view.dart';
-
+import 'dashboard.dart';
+import 'login.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         textTheme: Theme.of(context).textTheme.apply(fontFamily: 'Open Sans'),
       ),
-      home: HomeView(),
+      home: Login(),
     );
   }
 }
